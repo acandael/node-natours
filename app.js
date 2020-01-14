d231a29e0b11f64d4cd63a899317ba4e66876e38
acandael <info@anthonycandaele.com>
2020-01-14 14:36:14 +0100

    implement factory handler function

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 // 1: Global Middlewares
 // Set Security HTTP Headers
@@ -70,6 +71,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 400));
